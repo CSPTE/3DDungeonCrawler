71c3791c67b12e299fbddd7e12fc195c46241326
Patrik Csanyi <patriktibor@yahoo.com>
2022-11-18 16:51:44 +0000

    All soundfiles added
    self explanatory

--- RogueLike/Assets/Scripts/Target.cs
+++ RogueLike/Assets/Scripts/Target.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    public PlayerMotor pm;
 
     public void TakeDamage (float amount){
         health -= amount;
